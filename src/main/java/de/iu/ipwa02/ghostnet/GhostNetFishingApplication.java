7de434d809913a777bd9b85701cfab8eb2181f98
Patryk Plonka <patryk.plonka@iu-study.org>
2026-05-27 13:54:39 +0200

    Initial Spring Boot project

---
+++ src/main/java/de/iu/ipwa02/ghostnet/GhostNetFishingApplication.java
@@ -0,0 +1,13 @@
+package de.iu.ipwa02.ghostnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostNetFishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostNetFishingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/iu/ipwa02/ghostnet/GhostNetFishingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iu.ipwa02.ghostnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostNetFishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
