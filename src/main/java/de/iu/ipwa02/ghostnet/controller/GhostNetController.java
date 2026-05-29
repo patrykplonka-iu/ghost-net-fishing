446d28e0112c9d3862101de454a5de44104a8d01
Patryk Plonka <patryk.plonka@iu-study.org>
2026-05-29 10:33:12 +0200

    Speichern von Geisternetzen ermöglichen

--- src/main/java/de/iu/ipwa02/ghostnet/controller/GhostNetController.java
+++ src/main/java/de/iu/ipwa02/ghostnet/controller/GhostNetController.java
@@ -1,16 +1,30 @@
 package de.iu.ipwa02.ghostnet.controller;
 
 import de.iu.ipwa02.ghostnet.model.GhostNet;
+import de.iu.ipwa02.ghostnet.service.GhostNetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GhostNetController {
+
+    private final GhostNetService ghostNetService;
+
+    public GhostNetController(GhostNetService ghostNetService) {
+        this.ghostNetService = ghostNetService;
+    }
 
     @GetMapping("/ghostnets/new")
     public String showReportForm(Model model) {
         model.addAttribute("ghostNet", new GhostNet());
         return "report-net";
+    }
+
+    @PostMapping("/ghostnets")
+    public String saveGhostNet(GhostNet ghostNet) {
+        ghostNetService.saveGhostNet(ghostNet);
+        return "redirect:/ghostnets/new";
     }
 }
