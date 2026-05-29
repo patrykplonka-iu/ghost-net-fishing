5d84c9f74a5ca5c5925424da2114ccd9bcabca48
Patryk Plonka <patryk.plonka@iu-study.org>
2026-05-29 08:18:07 +0200

    Startseite hinzufügen

---
+++ src/main/java/de/iu/ipwa02/ghostnet/controller/HomeController.java
@@ -0,0 +1,13 @@
+package de.iu.ipwa02.ghostnet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+}
